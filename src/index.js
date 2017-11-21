23ee9f55b2aee460e1ec1588df7ae94b93848897
Ezekiel Chopper <lcarsos@lcarsos.com>
2017-11-20 23:03:28 -0700

    moving app entrypoint components to module organization scheme

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import RankerRater from './components/rankerrater';
-import reducer from './reducers';
+import RankerRater from 'app/components/rankerrater';
+import reducer from 'reducers';
 
 const initial_state = {};
 
