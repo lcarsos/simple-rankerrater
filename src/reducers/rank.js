bbdc2570964d97fa21a6197de0e7cc8aaa09ee8a
lcarsos <lcarsos@lcarsos.com>
2018-03-01 00:08:51 -0700

    labeling connected subgraphs to make everything easier
    With labeled subgraphs I can show the ranking of connected subgraphs
    and make better decisions when picking cards out of the deck for
    ranking.
    
    I fp-ed hard tonight. Not entirely happy with how branch-y it is right
    now. But, I don't expect this solution (graph labeling) to live long. I'm just proud of
    it right now.
    
    Rankerrater is going to need to be a lot more cognizant of making chains
    rather than graphs, so labeling will probably not be a helpful solution
    as Rankerrater gets better at building rankings with fewer attempts at rank
    description.

--- src/reducers/rank.js
+++ src/reducers/rank.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { ADD_RANK_DESCRIPTOR } from 'rank/constants';
+import { REPLACE_DECK } from 'impexp/constants';
 
 const default_node = {
   in: [],
@@ -10,6 +11,8 @@ const default_node = {
 const default_state = {
   edge: [],
   node: {},
+  labels: {
+  },
 };
 
 const node = (state, {_in = [], _out = []}) => ({
@@ -23,18 +26,61 @@ const nodes = (state = {}, [idA, idB]) => ({
   [idB]: node( state[ idB ] || default_node, {_out: [idA]} ),
 });
 
+const inSubgraph = id => subgraph => subgraph[1].indexOf( id ) > -1;
+
 const rank = (state = default_state, action) => {
   switch (action.type) {
     case ADD_RANK_DESCRIPTOR: {
       const newEdge = [action.idA, action.idB];
 
       if ( state.edge.indexOf( newEdge ) === -1 ) {
-        return {
+        const graph = {
           edge: state.edge.concat( [newEdge] ),
           node: nodes(state.node, newEdge),
         };
+
+        const labelA = Object.entries(state.labels).find( inSubgraph( action.idA ) )[0],
+          labelB = Object.entries(state.labels).find( inSubgraph( action.idB ) )[0];
+        if ( labelA === labelB ) {
+          return {
+            ...graph,
+            labels: state.labels,
+          };
+        }
+        return {
+          ...graph,
+          labels: Object.entries(state.labels).reduce(
+            (labels, [key, val]) => {
+              if ( key === labelA ) {
+                return {
+                  ...labels,
+                  [key]: val.concat( state.labels[labelB] ).sort(),
+                };
+              } else if ( key === labelB ) {
+                return labels;
+              }
+              return {
+                ...labels,
+                [key]: val,
+              };
+            },
+            {}
+          ),
+        };
       }
       return state;
+    }
+    case REPLACE_DECK: {
+      return {
+        ...default_state,
+        labels: [ ...Array(action.deck.length).keys() ].reduce(
+          (a, v) => ({
+            ...a,
+            [v]: [v], // use the value of v as the key, to an array with v as the only element
+          }),
+          {}
+        ),
+      };
     }
     default:
       return state;
